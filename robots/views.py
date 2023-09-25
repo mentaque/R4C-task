22660b7f83aa6088f2626c9a0ee1b6d7c25133b3
mentaque <mentaque322@gmail.com>
2023-09-25 17:24:28 +0300

    second task

--- robots/views.py
+++ robots/views.py
@@ -1,5 +1,10 @@
-from django.http import JsonResponse
+import os
+from datetime import datetime, timedelta
+
+from django.db.models import Count
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from openpyxl.workbook import Workbook
 
 from .forms import RobotForm
 import json
@@ -33,3 +38,39 @@ def create_robot(request):
 
     response_data = {'error': 'Method not allowed'}
     return JsonResponse(response_data, status=405)
+
+
+def download_excel(request):
+    workbook = Workbook()
+
+    today = datetime.today()
+    start_of_week = today - timedelta(days=today.weekday() + 7)
+
+    models = Robot.objects.values('model').distinct()
+
+    for model_info in models:
+        model = model_info['model']
+        worksheet = workbook.create_sheet(title=model)
+        worksheet.append(['Модель', 'Версия', 'Количество за неделю'])
+
+        robots = Robot.objects.filter(
+            model=model, created__gte=start_of_week
+        ).values('version').annotate(total_quantity=Count('version'))
+
+        for robot in robots:
+            worksheet.append([model, robot['version'], robot['total_quantity']])
+
+    default_sheet = workbook.get_sheet_by_name('Sheet')
+    workbook.remove(default_sheet)
+
+    file_path = 'robots-info.xlsx'
+
+    workbook.save(file_path)
+
+    with open(file_path, 'rb') as excel_file:
+        response = HttpResponse(excel_file.read(), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename="robots-info.xlsx"'
+
+    os.remove(file_path)
+
+    return response
